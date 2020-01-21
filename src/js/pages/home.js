1e0e042cb1b045e4bbd24fcc1371102688ecadc3
Kencana <kencana@C02XKBGLJG5H.local>
2020-01-21 12:04:28 +0800

    GH-6 add(what-do-you-seek): add functionality for dropdown - should treat it as drop down when screen < 1024px

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -4,18 +4,44 @@ import { debounce } from '../utils';
 
 class Home {
   constructor() {
-    this._ELEMENTS_CLASSES = {
-      clients: '.tuf-home-clients__list',
-      testimonials: '.tuf-home-testimonials__list',
-      what_we_do: '.tuf-home-what-we-do__list',
-      seek_label: '.tuf-home-what-do-you-seek__content__navigation__label',
-      seek_navigation: '.tuf-home-what-do-you-seek__content__navigation-wrapper',
-      seek_navigation_visible: '.tuf-home-what-do-you-seek__content__navigation-wrapper--visible'
+    this._CONST = {
+      clients_selector: '.tuf-home-clients__list',
+      testimonials_selector: '.tuf-home-testimonials__list',
+      what_we_do_selector: '.tuf-home-what-we-do__list',
+      seek_label_selector: '.tuf-home-what-do-you-seek__content__navigation__label',
+      seek_navigation_selector: '.tuf-home-what-do-you-seek__content__navigation-wrapper',
+      seek_navigation_visible_class: 'tuf-home-what-do-you-seek__content__navigation-wrapper--visible',
+      seek_item_selector: '.tuf-home-what-do-you-seek__content__navigation__item'
     };
 
     this._whatWeDoCarousel = null;
     this._testimonialCarousel = null;
     this._clientsCarousel = null;
+
+    this._seekLabelElem = document.querySelector(
+      this._CONST.seek_label_selector
+    );
+
+    this._seekNavigationElem = document.querySelector(
+      this._CONST.seek_navigation_selector
+    );
+
+    this._seekNavItems = document.querySelectorAll(
+      this._CONST.seek_item_selector
+    );
+
+    this._seekNavOpen = false;
+
+    this._showSeekHandler = (e) => this._showSeekEvent(e);
+    this._seekLabelClickHandler = () => this._toggleSeekOptions();
+    this._seekLabelEnterHandler = (e) => {
+      if (13 == e.keyCode) {
+        this._toggleSeekOptions();
+      }
+    };
+
+    this._initSeekNav();
+    this._bindSeekItemsOptions();
 
     this._initClientsCarousel();
 
@@ -29,12 +55,56 @@ class Home {
       'resize',
       debounce(() => {
         this._toggleWhatWeDoCarousel();
+        this._initSeekNav();
       }, 300)
     );
   }
 
+  _initSeekNav() {
+    // only enable carousel if width < 1024px
+    let mql = window.matchMedia('(min-width: 1024px)');
+
+    if (mql.matches) {
+      this._seekLabelElem.removeAttribute('tabindex');
+      // remove events on seek label
+      this._seekLabelElem.removeEventListener('click', this._seekLabelClickHandler);
+      this._seekLabelElem.removeEventListener('keydown', this._seekLabelEnterHandler);
+    } else {
+      this._seekLabelElem.setAttribute('tabindex', 0);
+      // add events on seek label
+      this._seekLabelElem.addEventListener('click', this._seekLabelClickHandler);
+      this._seekLabelElem.addEventListener('keydown', this._seekLabelEnterHandler);
+    }
+  }
+
+  _toggleSeekOptions() {
+    if (this._seekNavOpen) {
+      this._seekNavigationElem.classList.remove(
+        this._CONST.seek_navigation_visible_class
+      );
+    } else {
+      this._seekNavigationElem.classList.add(
+        this._CONST.seek_navigation_visible_class
+      );
+    }
+
+    this._seekNavOpen = !this._seekNavOpen;
+  }
+
+  _showSeekEvent(e) {
+    const type = e.target.getAttribute('data-type');
+
+    console.log(type);
+  }
+
+  _bindSeekItemsOptions() {
+    this._seekNavItems.forEach((elem) => {
+      elem.addEventListener('click', this._showSeekHandler);
+    });
+  }
+
   _initClientsCarousel() {
-    this._clientsCarousel = new Glide(this._ELEMENTS_CLASSES.clients, {
+    this._clientsCarousel = new Glide(this._CONST.clients_selector, {
       type: 'carousel',
       autoplay: 3000,
       perView: 8,
@@ -51,14 +121,14 @@ class Home {
   }
 
   _initTestimonialCarousel() {
-    this._testimonialCarousel = new Glide(this._ELEMENTS_CLASSES.testimonials, {
+    this._testimonialCarousel = new Glide(this._CONST.testimonials_selector, {
       type: 'carousel',
       perView: 1,
     }).mount();
   }
 
   _initWhatWeDoCarousel() {
-    this._whatWeDoCarousel = new Glide(this._ELEMENTS_CLASSES.what_we_do, {
+    this._whatWeDoCarousel = new Glide(this._CONST.what_we_do_selector, {
       type: "slider",
       focusAt: "center",
       perView: 3,

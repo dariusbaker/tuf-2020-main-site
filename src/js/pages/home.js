25e406ea2f142a60e80e7d27160ececac7ed8e25
Kencana <kencana@C02XKBGLJG5H.local>
2020-02-20 13:15:08 +0800

    GH-29 fix(home-clients): to set max width of bg on resize. to make sure that there is no cut off grid

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -12,11 +12,23 @@ import HomeInstagram from '../components/home-instagram';
 import HomeSeek from '../components/home-seek';
 import HomeAva from '../components/home-ava';
 import Speckle from '../components/speckle';
+import { debounce } from '../utils.js';
 
 class Home {
   constructor() {
     this._CONST = {
-      clients_selector: '.clients__list'
+      clients_selector: '.clients__list',
+      clients_bg_selector: '.clients__list-bg',
+      carousel_grid: {
+        mobile: 20,
+        desktop: 40
+      },
+      carousel_grid_margin: {
+        mobile: 16,
+        desktop: 48
+      },
+      carousel_bg_bp: 1024,
+      base_font: 16
     };
 
     // initialise testimonials carousel
@@ -76,6 +88,32 @@ class Home {
     this._clientsCarousel = null;
 
     this._initClientsCarousel();
+
+    this._$clientBg = document.querySelector(this._CONST.clients_bg_selector);
+
+    this._resizeCarouselBg();
+
+    // add resize event
+    window.addEventListener(
+      'resize',
+      debounce(() => {
+        this._resizeCarouselBg();
+      }, 300)
+    );
+  }
+
+  _resizeCarouselBg() {
+    const mql = window.matchMedia(`(min-width: ${this._CONST.carousel_bg_bp}px)`);
+
+    const gridSize = mql.matches ? this._CONST.carousel_grid.desktop : this._CONST.carousel_grid.mobile;
+
+    let marginSize = mql.matches ? this._CONST.carousel_grid_margin.desktop : this._CONST.carousel_grid_margin.mobile;
+
+    const maxBgWidth = document.body.offsetWidth - (marginSize * 2);
+
+    const maxGrid = Math.floor(maxBgWidth / gridSize);
+
+    this._$clientBg.style.maxWidth = `${(((maxGrid * gridSize) + 1) / this._CONST.base_font)}rem`;
   }
 
   _initClientsCarousel() {

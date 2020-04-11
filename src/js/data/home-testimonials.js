03c7777f84b9b9b3ae92b349e9b5f3fdae45e594
Darius Baker <funkage@gmail.com>
2020-04-11 21:03:26 +0800

    refactor(home): replace placeholder testimonials; closes #56

--- src/js/data/home-testimonials.js
+++ src/js/data/home-testimonials.js
@@ -1,10 +1,22 @@
 export const HOME_TESTIMONIALS_DATA = [
   {
-    name: 'Zeus the mighty pug',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur at orci eu scelerisque. Aenean in eros in turpis hendrerit ultrices.'
+    name: 'Triston O.<br>Director, Asher',
+    content: 'We’ve become a lot more nimble and competitive in the market thanks to your expertise in identifying our brand differentiators, and I’m immensely grateful for that.'
   },
   {
-    name: 'Zorro the miniature pinscher',
-    content: 'Sed pretium ullamcorper hendrerit. Integer aliquam urna at neque sodales maximus. Cras porta orci eget hendrerit lacinia. '
+    name: 'Andy C.<br>Digital Sales Enablement, AIA',
+    content: 'Very satisfied from the start to the end of the project. TUF has been very responsive to our feedback and questions raised along the way. Their creative approach and style is really unique. Through close collaboration, the results achieved was outstanding!'
+  },
+  {
+    name: 'Ida R.<br>Corp. Comms Manager, Allianz',
+    content: 'TUF was very attentive to our needs, and gave useful advice and guidance along the way. Needless to say, the project launched successfully resulting in acquiring more projects from other departments.'
+  },
+  {
+    name: 'Raphael H.<br>Regional Commercial Lead, Amgen',
+    content: 'The logos look good and in the mould of the excellent capability of TUF. Thanks!'
+  },
+  {
+    name: 'Lorraine C.<br>Head, Talent and Organisational Development (HR), AIA',
+    content: 'Thanks so much TUF, appreciate your efforts and partnership!'
   }
 ];
